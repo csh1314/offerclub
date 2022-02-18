5e77a716d8b89c4c6afa7b1c4d7046b6d92bb0f2
csh1314 <cshsb@qq.com>
2022-02-18 17:44:16 +0800

    fix: 不能关注自己

--- oc-server/src/web/middleware/checkMiddleware.js
+++ oc-server/src/web/middleware/checkMiddleware.js
@@ -14,6 +14,11 @@ const checkSameUsername = async (ctx, next) => {
 
 const checkFollowValid = async (ctx, next) => {
   const uid = ctx.userInfo._id.toString()
+  if(uid === ctx.request.body.id) {
+    const error = new Error('can not follow yourself')
+    error.status = 400
+    throw error
+  }
   const isFollow = await followService.isFollow(uid, ctx.request.body.id)
   if(isFollow) {
     const error = new Error('already follow')
